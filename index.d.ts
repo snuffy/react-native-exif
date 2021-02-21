ce676ad695e694fc685e441fb0b887f93547be16
xiao <super.xhmm@gmail.com>
2021-02-21 22:53:22 +0800

    change: export some types

--- index.d.ts
+++ index.d.ts
@@ -1,4 +1,4 @@
-interface ExifInfo {
+export interface ExifInfo {
   imageWidth: number;
   imageHeight: number;
   orientation: number;
@@ -6,11 +6,20 @@ interface ExifInfo {
   exif: Record<string, any>;
 }
 
+export interface LatLong {
+  latitude: number;
+  longitude: number;
+}
+
 interface Exif {
   getExif(uri: string): Promise<ExifInfo>;
+
   getExifWithLocalIdentifier(localIdentifier: string): Promise<ExifInfo>;
-  getLatLong(uri: string): Promise<{ latitude: number; longitude: number }>;
+
+  getLatLong(uri: string): Promise<LatLong>;
+
   getSize(uri: string): Promise<number>;
 }
+
 declare const Exif: Exif;
 export default Exif;
